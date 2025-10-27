5bfd38f8c67672a82a171fedaf4ecddcee5c06d7
harsh-pinjan <harshpinjan02@gmail.com>
2025-10-27 20:34:49 +0530

    Update qa_chain.py

--- src/qa_chain.py
+++ src/qa_chain.py
@@ -4,11 +4,9 @@ from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, pipeline
 from src.vector_store import load_faiss_vector_store
 
 def build_qa_chain():
-    # Load FAISS
     vector_store = load_faiss_vector_store()
     retriever = vector_store.as_retriever(search_kwargs={"k": 2})
 
-    # Load local HuggingFace model
     model_name = "google/flan-t5-small"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
@@ -16,7 +14,7 @@ def build_qa_chain():
     pipe = pipeline("text2text-generation", model=model, tokenizer=tokenizer, max_length=512)
     llm = HuggingFacePipeline(pipeline=pipe)
 
-    # Build QA chain
+    
     qa_chain = RetrievalQA.from_chain_type(
         llm=llm,
         retriever=retriever,
